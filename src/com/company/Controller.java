e38a086fdd668141343f8f8c73b43ffdf8c56146
PratamaAgung <apratamamia@gmail.com>
2017-06-17 15:33:49 +0700

    add some documentation

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -3,12 +3,21 @@ package com.company;
 public class Controller {
     private Learner learner;
 
+    /**
+     * Constructor of class controller
+     */
     public Controller(){
         learner = new Learner();
         learner.loadDataset("data/spam_training.arff");
         learner.evaluate();
         learner.learn();
     }
+
+    /**
+     * Method to test a mesage as spam or not
+     * @param message message to be tested
+     * @return classification of the message
+     */
     public int test(String message){
         SpamClassifier spamClassifier = new SpamClassifier(message);
         spamClassifier.makeInstance();
